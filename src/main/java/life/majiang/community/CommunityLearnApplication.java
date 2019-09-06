a2812b8a22d2c71d2194b8b8897713c55073cf31
haominglfs <haominglfs@163.com>
2019-09-06 09:53:03 +0800

    init project

---
+++ src/main/java/life/majiang/community/CommunityLearnApplication.java
@@ -0,0 +1,13 @@
+package life.majiang.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityLearnApplication.class, args);
+	}
+
+}
